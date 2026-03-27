1278eba8cd860acbe8be8520aead415c1d1cd542
Vladyslav Lubenskyi <Vladyslav.lubenskyi@teamdev.com>
2026-03-27 15:13:25 +0100

    Update the benchmark

--- benchmark/01-empty-app/mobrowser/src/main/index.ts
+++ benchmark/01-empty-app/mobrowser/src/main/index.ts
@@ -1,8 +1,9 @@
-import { app } from '@mobrowser/api';
+import { app, BrowserWindow } from '@mobrowser/api';
 
-const win = app.createWindow()
-win.browser.loadUrl(app.url)
-win.setSize({ width: 800, height: 600 })
+const win = new BrowserWindow({
+  size: { width: 800, height: 600 },
+  url: app.url,
+})
 win.show()
 
 console.log('App started and loaded !')
--- benchmark/02-empty-app-frameless/mobrowser/src/main/index.ts
+++ benchmark/02-empty-app-frameless/mobrowser/src/main/index.ts
@@ -1,9 +1,14 @@
-import { app } from '@mobrowser/api';
+import { app, BrowserWindow } from '@mobrowser/api';
 
-const win = app.createWindow()
-win.browser.loadUrl(app.url)
-win.setSize({ width: 800, height: 600 })
-win.setWindowTitlebarVisible(false)
+const win = new BrowserWindow({
+  size: { width: 800, height: 600 },
+  url: app.url,
+  windowTitleVisible: false,
+  windowTitlebarVisible: false,
+})
+win.setWindowButtonVisible('close', false)
+win.setWindowButtonVisible('minimize', false)
+win.setWindowButtonVisible('zoom', false)
 win.show()
 
 console.log('App started and loaded !')
