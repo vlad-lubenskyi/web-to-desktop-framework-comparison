94043d1b035cfd8a1b13bed33ac63abc8d04efcb
Vladyslav Lubenskyi <Vladyslav.lubenskyi@teamdev.com>
2026-04-07 16:10:49 +0200

    Update how we hide the title bar

--- benchmark/02-empty-app-frameless/mobrowser/src/main/index.ts
+++ benchmark/02-empty-app-frameless/mobrowser/src/main/index.ts
@@ -2,10 +2,10 @@ import { app, BrowserWindow } from '@mobrowser/api';
 
 const win = new BrowserWindow({
   size: { width: 800, height: 600 },
-  url: app.url,
-  windowTitleVisible: false,
-  windowTitlebarVisible: false,
+  url: app.url
 })
+win.setWindowTitleVisible(false)
+win.setWindowTitlebarVisible(false)
 win.setWindowButtonVisible('close', false)
 win.setWindowButtonVisible('minimize', false)
 win.setWindowButtonVisible('zoom', false)
